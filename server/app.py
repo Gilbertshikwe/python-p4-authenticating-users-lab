ef584164db354ca77004dc5199e747a51598b3b8
Gilbert <shikweg@gmail.com>
2024-04-12 21:42:44 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -2,7 +2,6 @@
 
 from flask import Flask, make_response, jsonify, request, session
 from flask_migrate import Migrate
-from flask_restful import Api, Resource
 
 from models import db, Article, User
 
@@ -16,42 +15,51 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-api = Api(app)
+@app.route('/clear', methods=['DELETE'])
+def clear_session():
+    session['page_views'] = None
+    session['user_id'] = None
+    return {}, 204
 
-class ClearSession(Resource):
+@app.route('/articles', methods=['GET'])
+def index_article():
+    articles = [article.to_dict() for article in Article.query.all()]
+    return jsonify(articles), 200
 
-    def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
+@app.route('/articles/<int:id>', methods=['GET'])
+def show_article(id):
+    session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
+    session['page_views'] += 1
 
-        return {}, 204
+    if session['page_views'] <= 3:
+        article = Article.query.filter(Article.id == id).first()
+        return jsonify(article.to_dict()), 200
 
-class IndexArticle(Resource):
-    
-    def get(self):
-        articles = [article.to_dict() for article in Article.query.all()]
-        return articles, 200
+    return {'message': 'Maximum pageview limit reached'}, 401
 
-class ShowArticle(Resource):
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.get_json().get('username')
+    user = User.query.filter_by(username=username).first()
+    if user:
+        session['user_id'] = user.id
+        return jsonify(user.to_dict()), 200
+    else:
+        return {'error': 'Invalid username'}, 401
 
-    def get(self, id):
-        session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
-        session['page_views'] += 1
+@app.route('/logout', methods=['DELETE'])
+def logout():
+    session.pop('user_id', None)
+    return {}, 204
 
-        if session['page_views'] <= 3:
-
-            article = Article.query.filter(Article.id == id).first()
-            article_json = jsonify(article.to_dict())
-
-            return make_response(article_json, 200)
-
-        return {'message': 'Maximum pageview limit reached'}, 401
-
-api.add_resource(ClearSession, '/clear')
-api.add_resource(IndexArticle, '/articles')
-api.add_resource(ShowArticle, '/articles/<int:id>')
-
+@app.route('/check_session', methods=['GET'])
+def check_session():
+    user_id = session.get('user_id')
+    if user_id:
+        user = User.query.get(user_id)
+        return jsonify(user.to_dict()), 200
+    else:
+        return {}, 401
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
